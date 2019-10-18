7e7c04467a7f4246cc64962a47405130372d8291
jinhao <mailjinhao@163.com>
2019-10-18 14:06:51 +0800

    sub succeed

--- mqtt.py
+++ mqtt.py
@@ -1,31 +1,56 @@
-import sys
-import os
-import paho.mqtt.client as mqtt
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: v1.0
+@author: tutu
+@license:
+@contact:
+@software: PyCharm
+@file:
+@time:
+@describe: mqtt demo
+"""
 
+#import sys
+#import os
+import paho.mqtt.client as mqtt
+import time
 
 HOST = "127.0.0.1"
 PORT = 1883
-mqtt_client = mqtt.Client()
+client_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+mqtt_client = mqtt.Client("PY_Mqtt_Client-"+client_id, transport="tcp")
+topic = "test"
 
 def mqtt_connect():
-    mqtt_client._client_id = "PY_Mqtt_Client"
-    #mqtt_client.username_pw_set("Hello world")
+    print("start connect mqtt-server")
+    mqtt_client.username_pw_set("Hello world")
+
+    mqtt_client.on_connect = mqtt_connected
+    mqtt_client.on_log = mqtt_log
+
     mqtt_client.connect(HOST, PORT, 60)
-    mqtt_client.on_message = sub_getmessage
-    #mqtt_client.on_connect = mqtt_connect
     mqtt_client.loop_forever()
 
 def sub_getmessage(client, userdata, msg):
     print(msg.topic + ":" + str(msg.payload))
 
-#def mqtt_connect():
-    #mqtt_subscribe()
+def mqtt_connected(client, userdata, flags, rc):
+    print("connecte succeed")
+    mqtt_subscribe()
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribe succeed")
 
 def mqtt_subscribe():
-    mqtt_client.subscribe('test', 0)
+    mqtt_client.subscribe(topic, 0)
+    mqtt_client.on_message = sub_getmessage
+    mqtt_client.on_subscribe = on_subscribe
+
+def mqtt_log(client, userdata, level, buf):
+    print(buf)
 
 if __name__ == "__main__":
     mqtt_connect()
-    mqtt_subscribe()
     while True:
         pass
